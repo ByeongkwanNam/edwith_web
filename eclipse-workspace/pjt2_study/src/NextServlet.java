f007720cee83dc44a540b8781d2b5af346816c7c
Byeongkwan Nam <jangu3000@gmail.com>
2019-08-16 13:47:35 +0900

    pjt2 - jsp and servlet

---
+++ eclipse-workspace/pjt2_study/src/NextServlet.java
@@ -0,0 +1,42 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class NextServlet
+ */
+@WebServlet("/next")
+public class NextServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public NextServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		int number = (int) request.getAttribute("number");
+		out.println("<html><body>");
+		for (int i=0; i<number; i++) {
+			out.println("Hello<br>");
+		}
+		out.println("</body></html>");
+	}
+
+
+}
